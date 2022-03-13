7264b358d27314683113c4bd9501df8a3cee00dc
Shubham <shubham.21920081@viit.ac.in>
2022-03-13 13:36:19 +0530

    added body-parser & allowed CORS

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-//const res = require("express/lib/response");
 const app = express();
 
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require("./api/routes/products");
-const orderRoutes = require("./api/routes/orders")
+const orderRoutes = require("./api/routes/orders");
+const { json } = require("body-parser");
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next)=> {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method=== 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({});
+    }
+    next();
+})
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
