6e34c9656b14da5f1424605791e1b50b2b50d664
Shubham Chavan <55504400+algokidd@users.noreply.github.com>
2022-06-01 19:59:47 +0530

    removed swagger ui

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ app.use("/orders", orderRoutes);
 app.use("/user", userRoutes);
 
 
-// app.use((req, res, next) => {
-//     const error = new Error('Not Found!');
-//     error.status = 404;
-//     next(error);
-// });
+app.use((req, res, next) => {
+    const error = new Error('Not Found!');
+    error.status = 404;
+    next(error);
+});
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
